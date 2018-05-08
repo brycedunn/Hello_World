d478ce6094db840356b29a48d583d04375ecc9a9
Bryce <34713702+brycedunn@users.noreply.github.com>
2018-05-07 20:50:28 -0400

    Update program.py

--- program.py
+++ program.py
@@ -1,3 +1,5 @@
+# 7 May 2018
+"""This is a simple Hello World app. User inputs their name and is greeted by a line of text"""
 print('+-----------------+')
 print('       HELLO')
 print('+-----------------+')
