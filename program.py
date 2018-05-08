f50a224e6df7bac9cdb8795372a7ef789473739c
Bryce <34713702+brycedunn@users.noreply.github.com>
2018-05-07 20:48:43 -0400

    Add files via upload
    Very simple Hello World app. User inputs name and is greeted with a line of text.

---
+++ program.py
@@ -0,0 +1,7 @@
+print('+-----------------+')
+print('       HELLO')
+print('+-----------------+')
+user_input = input('What is your name? ')
+greeting = 'Nice to meet you ' + user_input
+print(greeting)
+
